277077469e99e42e653cf34631d7c1c4ad15f6d1
lucaszxx5 <lucasd476@gmail.com>
2023-09-19 19:59:08 -0300

    Projeto 1

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const basePath = path.join(__dirname, 'templates');
+
+let pedidos = [];
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, resp) => {
+    resp.sendFile(`${basePath}/home.html`);
+});
+
+app.get('/pedido', (req, resp) => {
+    resp.sendFile(`${basePath}/pedido.html`);
+});
+
+app.post('/criarPedido', (req, resp) => {
+    const lanche = req.body.lanche;
+    const nome = req.body.nome;
+
+    const pedido = {
+        cliente: nome,
+        lanche: lanche
+    }
+
+    pedidos.push(pedido);
+    resp.redirect('/pedidos');
+});
+
+app.get('/pedidos', (req, resp) => {
+    resp.sendFile(`${basePath}/pedidos.html`);
+});
+
+app.get('/pedidos/:nome', (req, resp) => {
+    const nome = req.params.nome;
+    pedidos.forEach((pedido) => {
+        console.log(`Comparando ${pedido.cliente} com ${nome}`);
+        if (pedido.cliente.toLowerCase() === nome.toLowerCase()) {
+            console.log(`Cliente: ${pedido.cliente} | Lanche: ${pedido.lanche}`);
+        }
+    });
+    console.log(pedidos)
+    resp.end()
+})
+
+app.listen(5000);
